81997d8e8b32a188110807d281bde10e6aff8bad
MrTdevo <168947566+MrTdevo@users.noreply.github.com>
2024-08-08 23:03:56 -0400

    work in progress on notes.js

--- server.js
+++ server.js
@@ -7,19 +7,22 @@ const app = express();
 let PORT = process.env.PORT || 3001;
 app.use(express.json());
 
-app.get("/api/notes", (req, res) => {
-  console.log({ req, res });
-  const dbFile = fs.readFile(path.join(__dirname, "db/db.json"), {
+app.get("/api/notes", async (req, res) => {
+  // console.log({ req, res });
+  const dbFile = await fs.readFile(path.join(__dirname, "db/db.json"), {
     charset: "UTF8",
   });
-  console.log(dbFile);
+  // console.log(dbFile);
 
-  dbFile.then((db) => {
-    console.log(db);
-    // response.status.json goes here
-  });
+  const database = await res.status;
+  console.log(database);
 
-  res.status(200).send("This route get all the notes");
+  // dbFile.then((db) => {
+  //   console.log(db);
+  //   // response.status.json goes here
+  // });
+
+  res.status(200).send(dbFile);
 });
 
 app.listen(PORT, () => {
